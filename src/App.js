9c5ff158d8a1a056d5cd2c76f3105810b8eca557
aluappan <paula.panceiro@gmail.com>
2023-02-25 01:00:59 -0300

    nav bar implementada

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar.js"
+
+export default function App() {
+  return(
+    <div>
+      <NavBar />
+    </div>
+  )
+}
---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export default function NavBar() {
+    return(
+        <Nav>
+            <Tab>About</Tab>
+            <Tab>Entre em contato</Tab>
+            <Tab>Encomendas</Tab>
+            <Tab>Todos os produtos</Tab>
+        </Nav>
+    )
+}
+
+const Nav = styled.nav`
+    max-width: 35vw;
+    margin: 40px auto 0 auto;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Tab = styled.button`
+    font-family: 'Courier New';
+    background-color: rgb(204, 250, 157);
+    padding: 0.8%;
+    border: none;
+    border-radius: 5px;
+`
